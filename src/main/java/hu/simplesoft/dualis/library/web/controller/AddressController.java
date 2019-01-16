fccd2551a45b3152e809d93faaf60d6843f7462c
molnar <molnar.fanni@simplesoft.hu>
2019-01-16 08:12:51 +0100

    AddressController changes

--- src/main/java/hu/simplesoft/dualis/library/web/controller/AddressController.java
+++ src/main/java/hu/simplesoft/dualis/library/web/controller/AddressController.java
@@ -2,51 +2,44 @@ package hu.simplesoft.dualis.library.web.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import hu.simplesoft.dualis.library.service.component.AddressService;
 import hu.simplesoft.dualis.library.service.dto.AddressDto;
 import hu.simplesoft.dualis.library.service.exception.ServiceException;
 
+@RequestMapping
 @RestController
 public class AddressController {
 
     @Autowired
     private AddressService addressService;
     
-    @RequestMapping(value = "/createAddress", method = RequestMethod.POST)
-    public void createAddress(HttpServletRequest request,
-        HttpServletResponse response) throws ServiceException {
-        AddressDto addressDto = null;
+    @PostMapping("/createAddress")
+    public void createAddress(AddressDto addressDto) throws ServiceException {
         this.addressService.createAddress(addressDto);
     }
     
-    @RequestMapping(value = "/updateAddress", method = RequestMethod.POST)
-    public void updateAddress(HttpServletRequest request,
-        HttpServletResponse response) throws ServiceException {
-        AddressDto addressDto = null;
+    @PostMapping("/updateAddress")
+    public void updateAddress(AddressDto addressDto) throws ServiceException {
         this.addressService.updateAddress(addressDto);
     }
     
-    @RequestMapping(value = "/deleteAddress", method = RequestMethod.POST)
-    public void deleteAddress(Long addressId, HttpServletRequest request, 
-        HttpServletResponse response) throws ServiceException {
+    @PostMapping("/deleteAddress")
+    public void deleteAddress(Long addressId) throws ServiceException {
         this.addressService.deleteAddress(addressId);
     }
     
-    @RequestMapping(value = "/getAddressById", method = RequestMethod.GET)
-    public AddressDto getAddressById(Long addressId, HttpServletRequest request, 
-        HttpServletResponse response) throws ServiceException {
+    @GetMapping("/getAddressById")
+    public AddressDto getAddressById(Long addressId) throws ServiceException {
         return this.addressService.getAddressById(addressId);
     }
     
-    @RequestMapping(value = "/getAllAddresses", method = RequestMethod.GET)
+    @GetMapping("/getAllAddresses")
     public List<AddressDto> getAllAddresses()  {
         return this.getAllAddresses();
     }
